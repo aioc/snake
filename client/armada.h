d845265d011238eb7eb083e3a8c5d741acadde73
dahlukeh <dahlukeh@gmail.com>
2015-04-15 11:01:24 +1000

    Added in comments

--- client/armada.h
+++ client/armada.h
@@ -12,7 +12,7 @@ extern "C" {
 
 	// Limits of certain restrictions
 #define MAX_NUM_PLAYERS         4
-#define MAX_BOARD_SIZE          20
+#define MAX_BOARD_SIZE          40
 #define MAX_NAME_LENGTH			16
 
 	// Directions
@@ -42,49 +42,81 @@ extern "C" {
 	// The following must be implemented by the client:
 
 	/**
-	 *   This is called when your client connects to the server. You need to
-	 *   provide a name using setName and a colour with setColour.
+	 *   This is called when the client first logs on to the server. You should
+	 *   call setName(const char* name) to define the name of your AI and
+	 *   setColour(int r, int g, int b) to elect a preferred colour. Names can
+	 *   be up to 16 characters long and colour components are between 0 and
+	 *   255 inclusive.
 	 */
 	void clientRegister(void);
 	
 	// ******** These two functions are only called at the start of the game ********
 	
 	/**
-	 *   TODO:Describe
+	 * This is called at the start of a game. The argument numPlayers
+	 * specifies the number of players in this game. The argument boardSize
+	 * specifies the height/width of the game board, which is always square.
+	 * The argument playerID specifies your player ID throughout the game. Any
+	 * persistent data structures that you use throughout the course of a game
+	 * are best initialised here.
 	 */
-	void clientInit(int numPlayers, int boardSize, int pid);
+	void clientInit(int numPlayers, int boardSize, int playerID);
 	
 	/**
-	 *   TODO:Describe
+	 * This is called at the start of a game, after clientInit is called. It
+	 * will be called once for each player in the game, informing the client of
+	 * the positions of each of the ships in the player's armada. The argument
+	 * playerID specifies the player whose fleet the function call refers to.
+	 * The argument positions will be an array of points with length elements
+	 * (length will always be 5). struct point is a type which contains two
+	 * variables r and c indicating the row and column of the ship,
+	 * respectively.
 	 */
-	void clientStartPositions(int pid, int length, struct point* positions);
+	void clientStartPositions(int playerID, int length, struct point* positions);
 	
 	// ******** These four functions will be called *BEFORE* players take their turns for the round ********
 	
 	/**
-	 *   TODO:Describe
+	 * This is called before the client's turn. It will be called once for each
+	 * player in the game, informing the client of the new position of the head
+	 * of each player's fleet. The player is indicated by playerID and the new
+	 * head is indicated by position.
 	 */
-	void clientAddHead(int pid, struct point position);
+	void clientAddHead(int playerID, struct point position);
 
 	/**
-	 *   TODO:Describe
+	 * This is called before the client's turn. It will be called at most once
+	 * for each player in the game, informing the client of the movement of the
+	 * tail of each player's fleet. The player is indicated by playerID and the
+	 * previous position of tail is indicated by position
 	 */
-	void clientRemoveTail(int pid, struct point position);
+	void clientRemoveTail(int playerID, struct point position);
 	
 	/**
-	 *   TODO:Describe
+	 * This is called before the client's turn. It is called for each player
+	 * who was eliminated in the previous turn. The argument playerID indicates
+	 * the player id of the eliminated fleet.
 	 */
-	void clientPlayerDied(int pid);
+	void clientPlayerDied(int playerID);
 
 	/**
-	 *   TODO:Describe
+	 * This is called when a new abandoned ship appears on a square. The
+	 * argument position describes where the new abandoned ship has been
+	 * sighted.
 	 */
 	void clientFoodAdded(struct point position);
 	
 	// ******** This function will be called *WHEN* it is your turn ********
 
 	/**
-	 *   TODO: Describe
+	 * This is called when it's your turn to make a move. Do so by calling
+	 * makeMove(int direction). Argument direction should be used to specify
+	 * the direction that you wish your fleet's head to move in. (Note that
+	 * directions are defined in armada.h as UP, RIGHT, DOWN and LEFT) If you
+	 * call attempt to call makeMove() multiple times during your turn, only
+	 * the last value will be used. If you do not call makeMove() in
+	 * clientDoTurn(), your client will be dropped from the game and will be
+	 * recognised as having lost the game.
 	 */
 	void clientDoTurn(void);
 
@@ -93,28 +125,29 @@ extern "C" {
 	// The following are available to the client:
 
 	/**
-	 *   This will send to the server what you want your name to be. It must
-	 *   only contain A-Z, a-z, 1-9 and _. The length of the name should be at
-	 *   most 16 characters (not including the null terminating byte).
+	 * This will send to the server what you want your name to be. It must only
+	 * contain A-Z, a-z, 1-9 and _. The length of the name should be at most 16
+	 * characters (not including the null terminating byte).
 	 *
-	 *   This can only be called in clientRegister.
+	 * This can only be called in clientRegister.
 	 */
 	void setName(const char* name);
 
 	/**
-	 *   This will send to the server what colour you want to be. The values
-	 *   must be 0-255.
+	 * This will send to the server what colour you want to be. The values must
+	 * be 0-255.
 	 *
-	 *   This can only be called in clientRegister.
+	 * This can only be called in clientRegister.
 	 */
 	void setColour(int r, int g, int b);
 	
-	/**  
-	 *   TODO: Describe
+	/**
+	 * This will send to the server the move you want to make for this turn,
+	 * defined as the direction you want the head to move in.
 	 *
-	 *   This can only be called in clientDoTurn.
+	 * This can only be called in clientDoTurn.
 	 */
-	void makeMove(int d);
+	void makeMove(int direction);
 
 	/////////////////////////////////////////////////////////////////////
 
--- client/armada_example_client.cpp
+++ client/armada_example_client.cpp
@@ -2,8 +2,8 @@
 #include <cstdlib>
 #include "armada.h"
 
-#define BLANK			0
-#define FOOD			-1
+#define BLANK			-1
+#define FOOD			-2
 
 int dr[] = {-1, 0, 1, 0};
 int dc[] = {0, 1, 0, -1};
@@ -19,10 +19,10 @@ void clientRegister(void) {
 	setColour(255, 0, 0);
 }
 
-void clientInit(int numPlayers, int boardSize, int pid) {
+void clientInit(int numPlayers, int boardSize, int playerID) {
 	numP = numPlayers;
 	size = boardSize;
-	id = pid;
+	id = playerID;
 	int i, j;
 	for (i = 0; i < size; i++) {
 		for (j = 0; j < size; j++) {
@@ -31,33 +31,33 @@ void clientInit(int numPlayers, int boardSize, int pid) {
 	}
 }
 
-void clientStartPositions(int pid, int length, struct point* positions) {
+void clientStartPositions(int playerID, int length, struct point* positions) {
 	int i;
 	for (i = 0; i < length; i++) {
-		board[positions[i].r][positions[i].c] = pid + 1;
+		board[positions[i].r][positions[i].c] = playerID;
 	}
-	if (pid == id) {
+	if (playerID == id) {
 		myHead = positions[0];
 	}
 }
 
-void clientAddHead(int pid, struct point position) {
-	board[position.r][position.c] = pid + 1;
-	if (pid == id) {
+void clientAddHead(int playerID, struct point position) {
+	board[position.r][position.c] = playerID;
+	if (playerID == id) {
 		myHead = position;
 	}
 }
 
-void clientRemoveTail(int pid, struct point position) {
+void clientRemoveTail(int playerID, struct point position) {
 	board[position.r][position.c] = BLANK;
 }
 
-void clientPlayerDied(int pid) {
+void clientPlayerDied(int playerID) {
 	int i, j;
 	for (i = 0; i < size; i++) {
 		for (j = 0; j < size; j++) {
-			if (board[i][j] == pid + 1) {
-				board[i][j] = 0;
+			if (board[i][j] == playerID) {
+				board[i][j] = BLANK;
 			}
 		}
 	}	
@@ -68,17 +68,9 @@ void clientFoodAdded(struct point position) {
 }
 
 void clientDoTurn(void) {
-	int i;
-	int add = rand() % 4;
-	for (i = 0; i < 4; i++) {
-		int r = myHead.r + dr[(i + add) % 4];
-		int c = myHead.c + dc[(i + add) % 4];
-		if (r >= 0 && c >= 0 && r < size && c < size) {
-			if (board[r][c] <= 0) {
-				makeMove((i + add) % 4);
-				return;
-			}
-		}
-	}
-	makeMove(3);
+	makeMove(DOWN); // Quick! Run away! As far down as we can go!
+	// Note that this will either cause the head of the fleet to run
+	// into itself if the head begins upwards relative to the next
+	// ship in line, or it will eventually hit the bottom of the map if
+	// it doesn't hit another ship first.
 }
